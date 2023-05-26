37fe8b368588392e1b69735eaf9561637a471158
mohit2275 <mohit.chauhan2275@gmail.com>
2023-05-26 23:58:04 +0530

    This ia a simple countdown projects from specified date

---
+++ 1. Countdown Timer/script.js
@@ -0,0 +1,44 @@
+let endDate = "28 May 2023  11:57 PM"
+
+document.getElementById('end-date').innerText = endDate;
+
+let inputs = document.querySelectorAll("input")
+
+function clock() {
+    let end = new Date(endDate);
+    let now = new Date();
+    let diff = (end - now) / 1000;
+    //this will convert the milliseconds in seconds after dividing it from 1000.
+
+    if (diff < 0) return;
+
+    inputs[0].value = Math.floor(diff / 3600 / 24);
+    inputs[1].value = Math.floor((diff / 3600) % 24);
+    inputs[2].value = Math.floor((diff / 60) % 60);
+    inputs[3].value = Math.floor(diff % 60);
+}
+
+//initial call
+clock()
+
+setInterval(
+    () => {
+        clock()
+    }, 1000
+)
+
+
+
+
+
+
+
+
+
+
+
+/*
+1 day = 24 hr
+1 hr = 60 min = 3600 sec
+1 min = 60 sec
+*/
