588de1e4a646557243f54afadc30166fa6cc2fc7
mohit2275 <mohit.chauhan2275@gmail.com>
2023-06-01 23:19:57 +0530

    To do list app

---
+++ 6. To do list App/script.js
@@ -0,0 +1,36 @@
+let item = document.querySelector('#item');
+let toDoBox = document.querySelector('#to-do-box');
+
+item.addEventListener(
+    "keyup",
+    function (event) {
+        if (event.key === "Enter") {
+            addToDo(this.value);
+            this.value = "";
+        }
+    }
+)
+
+let addToDo = (item) => {
+    let listItem = document.createElement("li");
+    listItem.innerHTML = `
+    ${item}
+    <i class="fa-solid fa-xmark"></i>
+    `
+    toDoBox.appendChild(listItem)
+
+    listItem.addEventListener(
+        "click",
+        function () {
+            this.classList.toggle('done')
+        }
+    )
+    
+    listItem.querySelector('i').addEventListener(
+        "click",
+        function () {
+            listItem.remove()
+        }
+    )
+}
+
