a45c287cb22698c9d6d2195070de394a367c2d5c
mohit2275 <mohit.chauhan2275@gmail.com>
2023-07-03 23:42:20 +0530

    Clothing webpage using Fetch API

---
+++ 12. Fetch API/script.js
@@ -0,0 +1,18 @@
+
+let url = 'https://fakestoreapi.com/products';
+let product = document.getElementById('cart');
+
+fetch(url)
+    .then(mohit => mohit.json())
+    .then((data) => {
+        console.log(data);
+
+        data.forEach(element => {
+            product.innerHTML += `  <div id="one">
+            <h2>${element.title}</h2>
+            <img src="${element.image}" alt="">
+            <p>${element.price}</p>
+        </div>`
+        });
+    })
+
