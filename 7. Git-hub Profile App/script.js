040a598e7b3324615a59f532d8697b5a23b9c1c2
mohit2275 <mohit.chauhan2275@gmail.com>
2023-07-09 23:56:02 +0530

    This is simple project which can search the github profiles

---
+++ 7. Git-hub Profile App/script.js
@@ -0,0 +1,66 @@
+const APIURL = "https://api.github.com/users/";
+let main = document.getElementById("main");
+let search = document.querySelector("#search");
+
+setTimeout(() => {
+
+}, 2000)
+
+let getUser = (username) => {
+
+    fetch(APIURL + username)
+        .then((response) => response.json())
+        .then((data) => {
+
+            let card = `
+            <div id="card">            
+            <img src="${data.avatar_url}" alt="" id="photo" class="avtar">
+            <div class="user-info">
+                <h2>${data.name}</h2>
+                <p>${data.bio}</p>
+
+                <ul class="info">
+                    <li>${data.followers} <strong>Followers</strong></li>
+                    <li>${data.following} <strong>Following</strong></li>
+                    <li>${data.public_repos} <strong>Repos</strong></li>
+                </ul>
+
+                <div id="repos">
+                </div>
+            </div>
+        </div>`
+            main.innerHTML = card;
+
+            fetch(APIURL + username + "/repos")
+                .then(mohit => mohit.json())
+                .then((data) => {
+
+                    repos = document.getElementById('repos')
+                    data.forEach(element => {
+                        // console.log(element);
+                        repos.innerHTML += `
+                        <a href="${element.html_url}" class="repo" target="_blank">${element.name}</a>
+                        `
+                    });
+
+                })
+        })
+}
+
+
+let searchID = () => {
+    let search = document.querySelector("#search");
+    if (search.value != "") {
+        getUser(search.value);
+        search.value = "";
+    }
+}
+
+search.addEventListener(
+    "focusout",
+    function () {
+        searchID()
+    }
+)
+
+getUser('mohit2275');
