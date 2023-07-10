21f54c4aa9cf74336ecb90c855f4b3c4402a9bb0
mohit2275 <mohit.chauhan2275@gmail.com>
2023-07-10 22:47:47 +0530

    Weather App

---
+++ 9. Weather App/script.js
@@ -0,0 +1,40 @@
+const API_KEY = `3265874a2c77ae4a04bb96236a642d2f`
+
+// const API = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+
+// const IMG_URL = `https: //openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+
+let form = document.getElementById('ourForm');
+let search = document.getElementById('search');
+let details = document.getElementById('details');
+
+
+
+let getWeather = (city) => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then((data) => {
+            console.log(data);
+            if (data.cod == 404) {
+                details.innerHTML = `<h1>City Not Found</>`
+                return;
+            }
+
+            details.innerHTML = `
+            <img src= "https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="" id="pic">
+            <div class="text">
+                <h1>${data.main.temp}&degC</h1>
+                <h3>${data.weather[0].main}</h3>
+            </div>
+            `
+        })
+}
+
+let weather = () => {
+    let city = search.value;
+    getWeather(city);
+
+    return false;
+}
